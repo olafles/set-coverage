9398ca2ab18c309c5fc8ac27f70404b10e1d7b1b
Michał <106477318+Dethress@users.noreply.github.com>
2025-05-18 19:12:20 +0200

    Fix Covers Func (#9) (#10)
    #9
    
    Co-authored-by: Mike <none>

--- DataLoader.py
+++ DataLoader.py
@@ -4,12 +4,13 @@ import os
 
 
 class DataLoader:
-    file_path = ""
-    n: int
-    m: int
-    covers: list[list[int]]
-    costs: list[int]
-    reverse_covers: list[list[int]]
+    _file_path = ""
+    _n: int
+    _m: int 
+    _costs: list[int]
+    _element_covers: list[list[int]]
+    _subset_covers: list[list[int]]
+    
 
     def __init__(self, file_path: str) -> None:
         """Initialize with path to instance file
@@ -18,27 +19,28 @@ class DataLoader:
             file_path (str): Path to instance file
         """
         base_dir = os.path.dirname(__file__)
-        self.file_path = os.path.join(base_dir, "instances", file_path)
+        self._file_path = os.path.join(base_dir, "instances", file_path)
 
     def fetch_data(self) -> None:
         """Load data from file to class attributes using updated parser"""
-        with open(self.file_path, "r") as f:
+        with open(self._file_path, "r") as f:
             lines = f.read().splitlines()
             if not lines:
                 raise ValueError("File is empty or unreadable.")
 
         m, n = map(int, lines[0].split())
-        self.n = m
-        self.m = n
+        self._n = m +1
+        self._m = n +1
 
         costs = []
         idx = 1
         while len(costs) < n:
             costs.extend(map(int, lines[idx].split()))
             idx += 1
-        self.costs = costs
+        self._costs = costs
 
-        element_covers = []
+        element_covers = [[] for _ in range(self._n)]
+        current_element = 1
         while idx < len(lines):
             line = lines[idx].strip()
             if not line:
@@ -50,15 +52,19 @@ class DataLoader:
             while len(entries) < count:
                 idx += 1
                 entries += list(map(int, lines[idx].split()))
-            element_covers.append([x - 1 for x in entries])  # zero-based
+            #element_covers.append([x for x in entries])  # 1-based
+            element_covers[current_element].extend(entries)
+            current_element += 1
             idx += 1
+        self._element_covers = element_covers
 
-        covers = [set() for _ in range(n)]
-        for elem_index, col_list in enumerate(element_covers):
-            for col in col_list:
-                covers[col].add(elem_index + 1)  # switch back to 1-based
+        subset_covers = [set() for _ in range(n)]
+        for elem_index, subsets in enumerate(element_covers):
+            for subset in subsets:
+                subset_covers[subset - 1].add(elem_index + 1)  # switch back to 1-based
 
-        self.covers = [sorted(list(s)) for s in covers]
+        self._subset_covers = [sorted(list(s)) for s in subset_covers]
+
 
     def get_n(self) -> int:
         """Get number of elements that need to be covered
@@ -66,7 +72,7 @@ class DataLoader:
         Returns:
             int: Number of elements that need to be covered
         """
-        return self.n
+        return self._n
 
     def get_m(self) -> int:
         """Get number of subsets
@@ -74,32 +80,38 @@ class DataLoader:
         Returns:
             int: Number of subsets in instance
         """
-        return self.m
+        return self._m
+    
+    def get_element_covers(self) -> list[list[int]]:
+        """Return list of subsets covering each element
 
-    def get_covers(self) -> list[list[int]]:
+        Returns:
+            List[List[int]]: List of subsets covering each element
+        """
+        return self._element_covers
+
+    def get_subset_covers(self) -> list[list[int]]:
         """Return list of elements covered by each subset
 
         Returns:
             List[List[int]]: List of elements covered by each subset
         """
 
-        return self.covers
-
+        return self._subset_covers
+    
     def get_costs(self) -> list[int]:
         """Return cost of each subset
 
         Returns:
             List[int]: List of costs per subset
         """
-        return self.costs
-
-    def get_reverse_covers(self) -> list[list[int]]:
-        # Implement
-        return self.reverse_covers
+        return self._costs
 
 
 if __name__ == "__main__":
     print("Testing DataLoader...")
-    dl = DataLoader("scp41.txt")
+    dl = DataLoader("scp_toy.txt")
     dl.fetch_data()
-    print(dl.get_covers()[0])
+    for i in range(dl.get_n()):
+        print(f"Element {i} is covered by subsets: {dl.get_element_covers()[i]}")
+    a = 1 + None
